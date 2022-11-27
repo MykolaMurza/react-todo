74fbe08e4486f8a54f3de692bb0a2a476c511873
MykolaMurza <murza.na00@gmail.com>
2022-11-28 00:39:22 +0200

    Add tasks listing from json-server in list.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import {AddList, List, Tasks} from "./components/Component"
 import listSvg from "./assets/img/list.svg";
 
 function App() {
-    const [lists, setLists] = useState([]);
+    const [lists, setLists] = useState(null);
     const [colors, setColors] = useState(null);
 
     useEffect(() => {
-        axios.get("http://localhost:3001/lists?_expand=color").then(({data}) => {
+        axios.get("http://localhost:3001/lists?_expand=color&_embed=tasks").then(({data}) => {
             data = data.map(data => {
                 data["color"] = data.color.name;
                 return data;
@@ -29,17 +29,16 @@ function App() {
         <div className={"todo"}>
             <div className={"todo__sidebar"}>
                 <List items={[{
-                    svg: <img src={listSvg} alt={"Category list"}/>,
-                    name: "Categories"
+                    svg: <img src={listSvg} alt={"Category list"}/>, name: "Categories"
                 }]}/>
-                <List items={lists} onRemove={id => {
+                {lists ? (<List items={lists} onRemove={id => {
                     const newList = lists.filter(item => item.id !== id);
                     setLists(newList);
-                }} isRemovable={true}/>
+                }} isRemovable={true}/>) : ("Loading...")}
                 <AddList onAddList={onAddList} colors={colors}/>
             </div>
             <div className={"todo__tasks"}>
-                <Tasks/>
+                {lists && <Tasks list={lists[1]}/>}
             </div>
         </div>
     );
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -2,28 +2,30 @@ import React from "react";
 import "./Tasks.scss";
 import penSvg from "../../assets/img/pen.svg"
 
-const Tasks = () => {
+const Tasks = ({list}) => {
     return (
         <div className={"tasks"}>
-            <h2 className={"tasks__title"}>Hello, Category
+            <h2 className={"tasks__title"}> {list.name}
                 <img src={penSvg} alt={"Edit category name icon"}/>
             </h2>
 
             <div className={"tasks__items"}>
-                <div className={"tasks__items-row"}>
-                    <div className={"checkbox"}>
-                        <input type={"checkbox"} id={"check"}/>
-                        <label htmlFor={"check"}>
-                            <svg width="11" height="8" viewBox="0 0 11 8" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000"
-                                      strokeWidth="1.5"
-                                      strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                        </label>
+                {list.tasks.map(task =>
+                    <div key={task.id} className={"tasks__items-row"}>
+                        <div className={"checkbox"}>
+                            <input type={"checkbox"} id={`task-${task.id}`}/>
+                            <label htmlFor={`task-${task.id}`}>
+                                <svg width="11" height="8" viewBox="0 0 11 8" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000"
+                                          strokeWidth="1.5"
+                                          strokeLinecap="round" strokeLinejoin="round"/>
+                                </svg>
+                            </label>
+                        </div>
+                        <input readOnly value={task.text}/>
                     </div>
-                    <p>Learn something new, please!</p>
-                </div>
+                )}
             </div>
         </div>
     );
